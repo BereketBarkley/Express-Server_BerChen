9693913ecede1d69834cfa70772e5667e7bde2a4
emmettchen23 <emmett.chen23@trinityschoolnyc.org>
2022-12-14 13:24:03 -0500

    asddwq
    daygsd

--- server.js
+++ server.js
@@ -16,12 +16,6 @@ app.set('view engine', 'ejs'); //specify templating library
 //.............Define server routes..............................//
 //Express checks routes in the order in which they are defined
 
-app.get('/', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("index");
-});
-
 app.get('/play', function(request, response) {
     let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
     response.status(200);
@@ -31,7 +25,122 @@ app.get('/play', function(request, response) {
     });
 });
 
+
+app.get('/scores', function(request, response) {
+  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
+  let opponentArray=[];
+
+  //create an array to use sort, and dynamically generate win percent
+  for(name in opponents){
+    opponents[name].win_percent = (opponents[name].win/parseFloat(opponents[name].win+opponents[name].lose+opponents[name].tie) * 100).toFixed(2);
+    if(opponents[name].win_percent=="NaN") opponents[name].win_percent=0;
+    opponentArray.push(opponents[name])
+  }
+  opponentArray.sort(function(a, b){
+    return parseFloat(b.win_percent)-parseFloat(a.win_percent);
+  })
+
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("scores",{
+    opponents: opponentArray
+  });
+});
+
+
+app.get('/opponent/:opponentName', function(request, response) {
+  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
+
+  // using dynamic routes to specify resource request information
+  let opponentName = request.params.opponentName;
+
+  if(opponents[opponentName]){
+    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
+    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("opponentDetails",{
+      opponent: opponents[opponentName]
+    });
+
+  }else{
+    response.status(404);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("error", {
+      "errorCode":"404"
+    });
+  }
+});
+
+
+//------------------------------------------DEMO ABOVE----------------------------
+
+app.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("index");
+});
+
+app.get('/gradeSubmitTeacher', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("gradeSubmitTeacher");
+});
+
+app.get('/gradeSubmitStudent', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("gradeSubmitStudent");
+});
+
+app.get('/studentCreate', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("studentCreate");
+});
+
+app.get('/student/:studentName', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("studentProfile");
+  /*asds
+  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
+
+  // using dynamic routes to specify resource request information
+  let opponentName = request.params.opponentName;
+
+  if(opponents[opponentName]){
+    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
+    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("opponentDetails",{
+      opponent: opponents[opponentName]
+    });
+
+  }else{
+    response.status(404);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("error", {
+      "errorCode":"404"
+    });
+  }
+  */
+});
+
+app.get('/classScores', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("classScores");
+});
+
 app.get('/results', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("results");
+    /*
     let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
 
     //accessing URL query string information from the request object
@@ -79,59 +188,40 @@ app.get('/results', function(request, response) {
         "errorCode":"404"
       });
     }
+    */
 });
 
-app.get('/scores', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-  let opponentArray=[];
+app.post('/studentCreate', function(request, response) {
+  /*
+    let opponentName = request.body.opponentName;
+    let opponentPhoto = request.body.opponentPhoto;
+    if(opponentName&&opponentPhoto){
+      let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
+      let newOpponent={
+        "name": opponentName,
+        "photo": opponentPhoto,
+        "win":0,
+        "lose": 0,
+        "tie": 0,
+      }
+      opponents[opponentName] = newOpponent;
+      fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
 
-  //create an array to use sort, and dynamically generate win percent
-  for(name in opponents){
-    opponents[name].win_percent = (opponents[name].win/parseFloat(opponents[name].win+opponents[name].lose+opponents[name].tie) * 100).toFixed(2);
-    if(opponents[name].win_percent=="NaN") opponents[name].win_percent=0;
-    opponentArray.push(opponents[name])
-  }
-  opponentArray.sort(function(a, b){
-    return parseFloat(b.win_percent)-parseFloat(a.win_percent);
-  })
-
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("scores",{
-    opponents: opponentArray
-  });
+      response.status(200);
+      response.setHeader('Content-Type', 'text/html')
+      response.redirect("/opponent/"+opponentName);
+    }else{
+      response.status(400);
+      response.setHeader('Content-Type', 'text/html')
+      response.render("error", {
+        "errorCode":"400"
+      });
+    }
+    */
 });
 
-app.get('/opponent/:opponentName', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
 
-  // using dynamic routes to specify resource request information
-  let opponentName = request.params.opponentName;
-
-  if(opponents[opponentName]){
-    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
-    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
-
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("opponentDetails",{
-      opponent: opponents[opponentName]
-    });
-
-  }else{
-    response.status(404);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("error", {
-      "errorCode":"404"
-    });
-  }
-});
-
-app.get('/opponentCreate', function(request, response) {
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("opponentCreate");
-});
+//-----------------------------DEMO-----------------------------------------
 
 app.post('/opponentCreate', function(request, response) {
     let opponentName = request.body.opponentName;
