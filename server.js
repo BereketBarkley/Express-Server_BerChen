ed7d76cc55e5a5a3a5675462cd4f466a695bcdc8
emmettchen23 <emmett.chen23@trinityschoolnyc.org>
2023-01-11 15:33:42 -0500

    push

--- server.js
+++ server.js
@@ -16,65 +16,6 @@ app.set('view engine', 'ejs'); //specify templating library
 //.............Define server routes..............................//
 //Express checks routes in the order in which they are defined
 
-app.get('/play', function(request, response) {
-    let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("play", {
-      data: opponents
-    });
-});
-
-
-app.get('/scores', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-  let opponentArray=[];
-
-  //create an array to use sort, and dynamically generate win percent
-  for(name in opponents){
-    opponents[name].win_percent = (opponents[name].win/parseFloat(opponents[name].win+opponents[name].lose+opponents[name].tie) * 100).toFixed(2);
-    if(opponents[name].win_percent=="NaN") opponents[name].win_percent=0;
-    opponentArray.push(opponents[name])
-  }
-  opponentArray.sort(function(a, b){
-    return parseFloat(b.win_percent)-parseFloat(a.win_percent);
-  })
-
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("scores",{
-    opponents: opponentArray
-  });
-});
-
-
-app.get('/opponent/:opponentName', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-
-  // using dynamic routes to specify resource request information
-  let opponentName = request.params.opponentName;
-
-  if(opponents[opponentName]){
-    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
-    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
-
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("opponentDetails",{
-      opponent: opponents[opponentName]
-    });
-
-  }else{
-    response.status(404);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("error", {
-      "errorCode":"404"
-    });
-  }
-});
-
-
-//------------------------------------------DEMO ABOVE----------------------------
 
 app.get('/', function(request, response) {
   response.status(200);
@@ -257,8 +198,9 @@ app.post('/addStudentToClass', function(request, response) {
   if(studentName&&className){
     let classes = JSON.parse(fs.readFileSync('data/classes.json'));
     console.log(className, classes[className]["roster"]);
-
-    classes[className]["roster"].push(studentName);
+    if(classes[className]){
+      classes[className]["roster"].push(studentName);
+    }
     fs.writeFileSync('data/classes.json', JSON.stringify(classes));
 
     response.status(200);
@@ -319,34 +261,6 @@ app.post('/classCreate', function(request, response) {
 
 });
 
-//-----------------------------DEMO-----------------------------------------
-
-app.post('/opponentCreate', function(request, response) {
-    let opponentName = request.body.opponentName;
-    let opponentPhoto = request.body.opponentPhoto;
-    if(opponentName&&opponentPhoto){
-      let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-      let newOpponent={
-        "name": opponentName,
-        "photo": opponentPhoto,
-        "win":0,
-        "lose": 0,
-        "tie": 0,
-      }
-      opponents[opponentName] = newOpponent;
-      fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
-
-      response.status(200);
-      response.setHeader('Content-Type', 'text/html')
-      response.redirect("/opponent/"+opponentName);
-    }else{
-      response.status(400);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("error", {
-        "errorCode":"400"
-      });
-    }
-});
 
 // Because routes/middleware are applied in order,
 // this will act as a default error route in case of
